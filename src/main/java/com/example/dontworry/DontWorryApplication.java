511dbaef327ebc3c27d5c69d3f97b48102d5bc78
Seo, Myoungjin <sih5673@gmail.com>
2023-02-08 01:10:01 +0900

    start setting

---
+++ src/main/java/com/example/dontworry/DontWorryApplication.java
@@ -0,0 +1,13 @@
+package com.example.dontworry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontWorryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DontWorryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dontworry/DontWorryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dontworry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontWorryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
