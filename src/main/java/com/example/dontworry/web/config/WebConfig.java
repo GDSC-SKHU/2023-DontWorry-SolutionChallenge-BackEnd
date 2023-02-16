6384cbfd7624e0540f7d9a79884645e0d96b59f7
Seo, Myoungjin <sih5673@gmail.com>
2023-02-16 15:45:03 +0900

    feat : config

---
+++ src/main/java/com/example/dontworry/web/config/WebConfig.java
@@ -0,0 +1,35 @@
+package com.example.dontworry.web.config;
+
+import com.example.dontworry.web.argumentresolver.LoginMemberArgumentResolver;
+import com.example.dontworry.web.interceptor.LogInterceptor;
+import com.example.dontworry.web.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/signup", "/login", "/index",
+                        "/css/**", "/*.ico", "/error");
+    }
+}
