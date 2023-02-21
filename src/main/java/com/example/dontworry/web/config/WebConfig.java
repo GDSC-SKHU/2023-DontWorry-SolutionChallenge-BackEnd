2470fa2e1016ac33afba0314f0821fea83f35f54
Seo, Myoungjin <sih5673@gmail.com>
2023-02-21 14:13:48 +0900

    add : addCorsMappings

--- src/main/java/com/example/dontworry/web/config/WebConfig.java
+++ src/main/java/com/example/dontworry/web/config/WebConfig.java
@@ -5,6 +5,7 @@ import com.example.dontworry.web.interceptor.LogInterceptor;
 import com.example.dontworry.web.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,6 +15,7 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final long MAX_AGE_SECS = 3600;
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver());
@@ -31,5 +33,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/signup", "/login", "/index",
                         "/css/**", "/*.ico", "/error");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
     }
 }
