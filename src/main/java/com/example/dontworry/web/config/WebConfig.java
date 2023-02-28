aebe3fd95a98527a9638641913f776170e5e8b37
Seo, Myoungjin <sih5673@gmail.com>
2023-02-28 21:42:34 +0900

    refactor : cookie

--- src/main/java/com/example/dontworry/web/config/WebConfig.java
+++ src/main/java/com/example/dontworry/web/config/WebConfig.java
@@ -37,6 +37,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOriginPatterns("*")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
