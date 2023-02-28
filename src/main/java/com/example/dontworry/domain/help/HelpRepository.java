7b599279b0132f7720fcda22ebab7f88fc822853
Seo, Myoungjin <sih5673@gmail.com>
2023-02-28 21:35:50 +0900

    :fire:  Remove files

--- src/main/java/com/example/dontworry/domain/help/HelpRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dontworry.domain.help;
-
-import com.example.dontworry.domain.posts.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HelpRepository extends JpaRepository<Help,String> {
-}
--- src/main/java/com/example/dontworry/domain/violence/ViolenceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.dontworry.domain.violence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ViolenceRepository extends JpaRepository<Violence,String> {
-}
