d18482ac8d81fa6413391f15ed12fb2714e4b3dc
Seo, Myoungjin <sih5673@gmail.com>
2023-03-01 12:12:05 +0900

    refactor : delete loginRedirect

--- src/main/java/com/example/dontworry/web/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/example/dontworry/web/interceptor/LoginCheckInterceptor.java
@@ -20,12 +20,12 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
 
         HttpSession session = request.getSession();
 
-        if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
-            log.info("미인증 사용자 요청");
-            //로그인으로 redirect
-            response.sendRedirect("/login?redirectURL=" + requestURI);
-            return false;
-        }
+//        if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+//            log.info("미인증 사용자 요청");
+//            //로그인으로 redirect
+//            response.sendRedirect("/login?redirectURL=" + requestURI);
+//            return false;
+//        }
 
         return true;
     }
