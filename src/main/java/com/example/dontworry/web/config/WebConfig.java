e5adb6da0bb9aa01256f7de31b04b16a96ae1507
Seo, Myoungjin <sih5673@gmail.com>
2023-03-08 00:55:08 +0900

    refactor : allowedOrigins add domain

--- src/main/java/com/example/dontworry/web/config/WebConfig.java
+++ src/main/java/com/example/dontworry/web/config/WebConfig.java
@@ -37,8 +37,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-//                .allowedOrigins("http://localhost:3000")
+//                .allowedOriginPatterns("*")
+                .allowedOrigins("https://dontworry.duckdns.org")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
