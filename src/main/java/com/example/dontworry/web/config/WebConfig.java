48d175def426d5bbdb2e8b86cfc91e79c2f2cd16
Seo, Myoungjin <sih5673@gmail.com>
2023-03-08 01:12:34 +0900

    refactor : domain

--- src/main/java/com/example/dontworry/web/config/WebConfig.java
+++ src/main/java/com/example/dontworry/web/config/WebConfig.java
@@ -38,8 +38,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
 //                .allowedOriginPatterns("*")
-                .allowedOrigins("https://dontworry.duckdns.org:3000"
-                ,"https://dontworry.duckdns.org/login:3000"," https://dontworry.duckdns.org/Main:3000")
+                .allowedOrigins("https://dontworry.duckdns.org:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
