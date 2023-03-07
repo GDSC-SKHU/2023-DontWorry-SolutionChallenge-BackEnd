57df4c73dc2bccfc70c82320bfc51ebb56823c0e
Seo, Myoungjin <sih5673@gmail.com>
2023-03-07 19:29:31 +0900

    refactor : jpa

--- src/main/java/com/example/dontworry/domain/posts/Posts.java
+++ src/main/java/com/example/dontworry/domain/posts/Posts.java
@@ -1,15 +1,19 @@
 package com.example.dontworry.domain.posts;
 
 import com.example.dontworry.domain.BaseTimeEntity;
+//import com.example.dontworry.domain.category.Category;
 import com.example.dontworry.domain.uploadFile.UploadFile;
 import com.example.dontworry.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Entity
@@ -19,20 +23,19 @@ import java.util.List;
 public class Posts extends BaseTimeEntity {
 
     @Id
-    @Column(nullable = false)
+    @Column(nullable = false,name = "POST_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name ="category")
     private String category;
 
-    @Column
-    @JsonManagedReference
-    @OneToMany(mappedBy = "posts")
-    private List<UploadFile> imageFiles;
+    @OneToMany
+    @JoinColumn(name = "POST_ID")
+    private List<UploadFile> files = new ArrayList<>();
 
     @Column(nullable = false)
     private String mainText;
