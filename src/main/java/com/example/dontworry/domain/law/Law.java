4a30444e3ec5585167123ce10a6554cef8cb1257
Seo, Myoungjin <sih5673@gmail.com>
2023-03-22 20:34:44 +0900

    refactor  : Law

--- src/main/java/com/example/dontworry/domain/law/Law.java
+++ src/main/java/com/example/dontworry/domain/law/Law.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Builder
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class Law {
     @Id
     @Column(nullable = false)
