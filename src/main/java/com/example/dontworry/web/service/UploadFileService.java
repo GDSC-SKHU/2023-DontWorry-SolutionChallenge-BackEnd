532d2d35561e44bbc57ed134c0b20676b87f0379
Seo, Myoungjin <sih5673@gmail.com>
2023-03-22 20:35:30 +0900

    refactor  : delete @Autowired

--- src/main/java/com/example/dontworry/web/service/UploadFileService.java
+++ src/main/java/com/example/dontworry/web/service/UploadFileService.java
@@ -4,6 +4,7 @@ import com.example.dontworry.domain.uploadFile.UploadFile;
 import com.example.dontworry.domain.uploadFile.UploadFileRepository;
 
 import com.example.dontworry.web.util.FileHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,18 +13,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UploadFileService {
-    @Autowired
-    private FileHandler fileHandler;
-    @Autowired
-    private UploadFileRepository uploadFileRepository;
+
+    private final FileHandler fileHandler;
+
+    private final UploadFileRepository uploadFileRepository;
     public List<UploadFile> addUploadFile(
             List<MultipartFile> files
 
     ) throws Exception {
+
         List<UploadFile> list = fileHandler.parseFileBoardPictureInfo(files);
 
+
         List<UploadFile> uploadFiles = new ArrayList<>();
+        System.out.print(list);
         for(UploadFile uploadFile : list) {
             uploadFiles.add(uploadFileRepository.save(uploadFile));
         }
